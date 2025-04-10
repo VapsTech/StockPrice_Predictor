90d0ed47524011127820cd2b92b6c1318e9445e4
Vaps <victoralbertopsoares@gmail.com>
2025-04-10 11:24:38 -0400

    final update

--- evaluation.py
+++ evaluation.py
@@ -56,24 +56,24 @@ for stock in stocks: #Iterating over each stock to be predicted by each model
     dates_test_sorted = pd.to_datetime(dates_test.sort_index())
 
     # # ---- LSTM Model ----
-    # Y_predictions = lstm_train_predict(X_train, Y_train, X_test_sorted)
+    Y_predictions = lstm_train_predict(X_train, Y_train, X_test_sorted)
 
-    # r2, mse = evaluate_model('LSTM', stock, y_test_sorted, Y_predictions)
+    r2, mse = evaluate_model('LSTM', stock, y_test_sorted, Y_predictions)
 
-    # results['lstm_r2'].append(r2)
-    # results['lstm_mse'].append(mse)
+    results['lstm_r2'].append(r2)
+    results['lstm_mse'].append(mse)
 
-    # plt.figure(figsize=(10, 6))
-    # plt.plot(dates_test_sorted, y_test_sorted.values, label='True Values', color='green')
-    # plt.plot(dates_test_sorted, Y_predictions, label='LSTM Predictions', color='purple')
-    # plt.title(f'{stock} - LSTM')
-    # plt.xlabel('Date')
-    # plt.ylabel('Stock Price')
-    # plt.legend()
-    # plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%Y'))
-    # plt.gca().xaxis.set_major_locator(mdates.YearLocator())
-    # plt.savefig(os.path.join('result/lstm_plots', f'{stock}_lstm.png'))
-    # plt.close()
+    plt.figure(figsize=(10, 6))
+    plt.plot(dates_test_sorted, y_test_sorted.values, label='True Values', color='green')
+    plt.plot(dates_test_sorted, Y_predictions, label='LSTM Predictions', color='purple')
+    plt.title(f'{stock} - LSTM')
+    plt.xlabel('Date')
+    plt.ylabel('Stock Price')
+    plt.legend()
+    plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%Y'))
+    plt.gca().xaxis.set_major_locator(mdates.YearLocator())
+    plt.savefig(os.path.join('result/lstm_plots', f'{stock}_lstm.png'))
+    plt.close()
 
     # ---- Random Forest Model ----
     Y_predictions = randomForest_train_predict(X_train, Y_train, X_test_sorted)
@@ -119,10 +119,10 @@ for stock in stocks: #Iterating over each stock to be predicted by each model
 print("Results:")
 
 # LSTM Average Results 
-# lstm_r2 = np.mean(results['lstm_r2'])
-# lstm_mse = np.mean(results['lstm_mse'])
-# print(f"LSTM Average R2 Score: {lstm_r2:.4f}")
-# print(f"LSTM Average Mean Squared Error: {lstm_mse:.4f}")
+lstm_r2 = np.mean(results['lstm_r2'])
+lstm_mse = np.mean(results['lstm_mse'])
+print(f"LSTM Average R2 Score: {lstm_r2:.4f}")
+print(f"LSTM Average Mean Squared Error: {lstm_mse:.4f}")
 
 # Random Forest Average Results
 randomForest_r2 = np.mean(results['randomForest_r2'])
